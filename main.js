b7a230139d5ca22b0701e6c45a4db2fc4b79491e
Mohammad Hunan Chughtai <mohammadhunan@gmail.com>
2023-03-12 21:38:20 -0400

    fix script

--- main.js
+++ main.js
@@ -1,46 +1,34 @@
 const sharp = require("sharp");
+const path = require("path");
 const fs = require("fs");
 
 const inputPath = "input.png";
-const outputPathBase = "output";
+const outputDir = "output";
+const outputSizes = [1024, 640, 320];
 
-// Define the desired output sizes
-const outputSizes = [
-  { width: 1024, height: 768 },
-  { width: 640, height: 480 },
-  { width: 320, height: 240 },
-];
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
 
-// Read the input image
-sharp(inputPath)
-  .webp({ lossless: true })
-  .toBuffer()
-  .then((outputBuffer) => {
-    // Loop through the desired output sizes
-    outputSizes.forEach((size, index) => {
-      // Calculate the output filename based on the output size and index
-      const outputFilename = `${outputPathBase}-${size.width}x${size.height}-${index}.webp`;
+async function convertImage(inputPath, outputPath, outputWidth) {
+  const inputBuffer = await fs.promises.readFile(inputPath);
+  const { width, height } = await sharp(inputBuffer).metadata();
+  const outputHeight = Math.round(outputWidth / (width / height));
+  const outputBuffer = await sharp(inputBuffer)
+    .resize(outputWidth, outputHeight, { fit: "contain" })
+    .webp({ lossless: true })
+    .toBuffer();
+  await fs.promises.writeFile(outputPath, outputBuffer);
+}
 
-      // Calculate the output width and height while maintaining the original aspect ratio
-      const { width, height } = sharp(inputPath).metadata();
-      const aspectRatio = width / height;
-      const outputWidth = size.width;
-      const outputHeight = Math.round(outputWidth / aspectRatio);
-
-      // Resize the image and write it to the output file
-      sharp(outputBuffer)
-        .resize(outputWidth, outputHeight)
-        .toFile(outputFilename, (err) => {
-          if (err) {
-            console.error(
-              `Error writing output file ${outputFilename}: ${err}`
-            );
-          } else {
-            console.log(`Successfully wrote output file ${outputFilename}`);
-          }
-        });
-    });
-  })
-  .catch((err) => {
-    console.error(`Error processing input file ${inputPath}: ${err}`);
-  });
+(async function () {
+  for (const outputWidth of outputSizes) {
+    const outputName = `${path.basename(
+      inputPath,
+      path.extname(inputPath)
+    )}_${outputWidth}w.webp`;
+    const outputPath = path.join(outputDir, outputName);
+    await convertImage(inputPath, outputPath, outputWidth);
+    console.log(`Converted ${inputPath} to ${outputPath}`);
+  }
+})();
